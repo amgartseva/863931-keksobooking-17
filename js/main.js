ddc87dcc265b2c0f3c04ef3c90906befc6150da8
Anastasia Gartseva <amgartseva@gmail.com>
2019-07-29 02:52:45 +0300

    Второе задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 var ads = [];
-var pin = document.querySelector('.map__pin');
+var pin = document.querySelector('.map__pin--main');
 var map = document.querySelector('.map');
 var mapPin = document.querySelector('.map__pins');
 var pinTemplate = document.getElementById('pin').content.querySelector('.map__pin');
+var adForm = document.querySelector('.ad-form');
+var adFormFieldsets = adForm.getElementsByTagName('FIELDSET');
+var mapFilterFieldset = document.querySelector('.map__filters').querySelector('.map__features');
+var mapFilterSelects = document.querySelector('.map__filters').getElementsByTagName('SELECT');
+var xMainPin = pin.offsetLeft + pin.clientWidth / 2;
+var yMainPin = pin.offsetTop + pin.clientHeight / 2;
+var address = document.getElementById('address');
 
 function randomInteger(min, max) {
   var random = Math.floor(Math.random() * (+max - +min)) + +min;
@@ -29,7 +36,6 @@ function generateData(ads) {
       y: randomInteger(130, 631)
     };
   };
-  console.log(ads[2]);
 };
 
 function getX() {
@@ -58,10 +64,6 @@ function getType() {
   }
 };
 
-generateData(ads);
-
-map.classList.remove('map--faded');
-
 function createDOM(ad) {
   var newPin = pinTemplate.cloneNode(true);
   newPin.style.left = ad.location.x + 'px';
@@ -73,8 +75,91 @@ function createDOM(ad) {
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < ads.length; i++) {
-  fragment.appendChild(createDOM(ads[i]));
+function drawElement(fragment) {
+  for (var i = 0; i < ads.length; i++) {
+    fragment.appendChild(createDOM(ads[i]));
+  };
+  mapPin.appendChild(fragment);
 };
 
-mapPin.appendChild(fragment);
+function onPinClick() {
+  map.classList.remove('map--faded');
+  generateData(ads);
+  drawElement(fragment);
+  adForm.classList.remove('ad-form--disabled');
+  mapFilterFieldset.removeAttribute('disabled');
+  for (var i = 0; i < mapFilterSelects.length; i++) {
+    mapFilterSelects[i].removeAttribute('disabled');
+  };
+  for (var i = 0; i < adFormFieldsets.length; i++) {
+    adFormFieldsets[i].removeAttribute('disabled');
+  };
+  pin.removeEventListener('mouseup', onPinClick);
+};
+
+pin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  function onMouseMove(moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    if (pin.offsetTop - shift.y < 0) {
+      pin.style.top = 0 + 'px';
+    } else if (pin.offsetTop - shift.y > mapPin.clientHeight) {
+      pin.style.top = (mapPin.clientHeight - pin.clientHeight / 2) + 'px';
+    } else {
+      pin.style.top = (pin.offsetTop - shift.y) + 'px';
+    };
+    if (pin.offsetLeft - shift.x < 0) {
+      pin.style.left = 0 + 'px';
+    } else if (pin.offsetLeft - shift.x > mapPin.clientWidth) {
+      pin.style.left = (mapPin.clientWidth - pin.clientWidth) + 'px';
+    } else {
+      pin.style.left = (pin.offsetLeft - shift.x) + 'px';
+    };
+  };
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    getPinCoordinates();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+function getPinCoordinates() {
+  var x = pin.offsetLeft + pin.clientWidth / 2;
+  var y = pin.offsetTop + pin.clientHeight;
+  address.value = x + ', ' + y;
+};
+
+mapFilterFieldset.setAttribute('disabled', 'disabled');
+
+for (var i = 0; i < mapFilterSelects.length; i++) {
+  mapFilterSelects[i].setAttribute('disabled', 'disabled');
+};
+
+for (var i = 0; i < adFormFieldsets.length; i++) {
+  adFormFieldsets[i].setAttribute('disabled', 'disabled');
+};
+
+address.value = xMainPin + ', ' + yMainPin;
+
+pin.addEventListener('mouseup', onPinClick);
